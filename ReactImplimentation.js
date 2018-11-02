834b4e449b7f073feb75322b7ab6d02d7c75af37
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-02 18:45:23 -0400

    reactimplementation example.

--- ReactImplimentation.js
+++ ReactImplimentation.js
@@ -1,3 +1,8 @@
 // html input to the Dom container
 
 <div id="like_button_container" ></div>
+
+
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.Render(e(likeButton), domContainer);
