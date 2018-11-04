ea997c62bc93b196bdec86095664d6ec596f4494
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-04 14:31:21 -0500

    "...."

--- react101-2.js
+++ react101-2.js
@@ -263,6 +263,8 @@ class Basketball extends React.Component {
 
 // Component composition is one of React's powerful features. When you work with React, it is important to start thinking about your user interface in terms of components like the App example in the last challenge. You break down your UI into its basic building blocks, and those pieces become the components.
 
+// There are two functional components defined in the code editor, called TypesOfFruit and Fruits. Take the TypesOfFruit component and compose it, or nest it, within the Fruits component. Then take the Fruits component and nest it within the TypesOfFood component. The result should be a child component, nested within a parent component, which is nested within a parent component of its own!
+
 const TypesOfFruit = () => {
     return (
       <div>
